1ac89d5ba810a88ce82a0bc25d3018c292e48336
Muhammad Usman <108360059+US24MAN2327@users.noreply.github.com>
2024-10-09 23:39:14 +0500

    Update model.py

--- model.py
+++ model.py
@@ -12,7 +12,11 @@ IMG = 255
 
 
 # Now you can load the model without needing the custom object scope
-resnet34 = load_model('seq.keras')
+
+import os
+model_path = os.path.join(os.getcwd(), 'seq.keras')
+resnet34 = load_model(model_path)
+
 
 # Print the model summary
 print(resnet34.summary())
